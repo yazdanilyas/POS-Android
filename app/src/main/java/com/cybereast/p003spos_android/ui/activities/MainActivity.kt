c4e86942bfd0eb78a36fe2f3eb1133e00425eea9
Yazdan <yazdan.ilyas@techswivel.com>
2021-07-04 14:50:11 +0500

    Added gradle files

---
+++ app/src/main/java/com/cybereast/p003spos_android/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.cybereast.p003spos_android.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cybereast.p003spos_android.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
