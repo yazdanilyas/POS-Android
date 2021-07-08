a7c5d7df90f20a20121a6b82f27f560e8d7585d6
PC <ranaased.ahmad@techswivel.com>
2021-07-08 23:26:28 +0500

    Completed Ledger Screen design completed

---
+++ app/src/main/java/com/cybereast/p003spos_android/ui/fragments/ledgerFragment/LedgerViewModel.kt
@@ -0,0 +1,7 @@
+package com.cybereast.p003spos_android.ui.fragments.ledgerFragment
+
+import androidx.lifecycle.ViewModel
+
+class LedgerViewModel : ViewModel() {
+    var mLedgerList: MutableList<Any> = ArrayList()
+}
